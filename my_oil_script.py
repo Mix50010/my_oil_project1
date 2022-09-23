7e295b142272437c019dc48fc25c14b123b39001
Mix50010 <44709738+Mix50010@users.noreply.github.com>
2022-09-23 22:01:42 +0700

    Add files via upload
    - added xlsx file creation

--- my_oil_script.py
+++ my_oil_script.py
@@ -1,5 +1,6 @@
 import platform, psutil
 from screeninfo import get_monitors
+from openpyxl import Workbook
 
 data = dict()
 
@@ -8,7 +9,16 @@ data['pc_name'] = platform.node()
 data['cpu'] = platform.machine()
 data['ram_size'] = round(psutil.virtual_memory().total / 1024 ** 3, 1)
 for i, monitor in enumerate(get_monitors()):
-    data[f'monitor_{i}'] = (monitor.width, monitor.height)
+    data[f'monitor_{i}'] = str((monitor.width, monitor.height))
 
 print(*map(lambda x: f'{x}: {data[x]}', data), sep='\n')
+
+wb = Workbook()
+ws = wb.active
+ws.title = "Platform info"
+for i, pair in enumerate(data.items(), 1):
+    key, value = pair
+    ws[f'A{i}'] = key
+    ws[f'B{i}'] = value
+wb.save('platform_info.xlsx')
 input('Press ENTER to escape')
